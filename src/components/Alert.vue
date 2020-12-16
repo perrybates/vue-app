bf36092743243c56886d988b32666b0cb536a393
Perry Bates <perryboateng08@gmail.com>
2020-12-16 15:25:03 +0000

    second vue-app commit

---
+++ src/components/Alert.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="alert " :class="mode" role="alert">
+        <slot></slot>
+    </div>
+            
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: [
+    'mode'
+  ],
+  
+}
+</script>
+
+<style scoped lang="scss">
+.primary {
+    color: #fff;
+    background-color: #084298;
+    border-color: #084298;
+}
+.success {
+    color: #fff;
+    background-color: #42b983;
+    border-color: #42b983;
+}
+.danger {
+    color: #fff;
+    background-color: #6e194c94;
+    border-color: #6e194c94;
+}
+
+
+.alert {
+    position: relative;
+    padding: 1rem 1rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+}
+</style> 
+
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.component("alert", require('./components/Alert.vue').default); 
+
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
